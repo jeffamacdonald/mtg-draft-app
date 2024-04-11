7fedb3829b7ad628336b270557980fb3191762aa
Jeff MacDonald <j.a.macdonald@gmail.com>
2024-04-10 22:28:26 -0700

    more styling changes

---
+++ app/components/display_card_component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class DisplayCardComponent < ViewComponent::Base
+  attr_reader :width
+
+  WIDTHS = {
+    sm: "col-md-4",
+    md: "col-md-6",
+    lg: "col-md-8"
+  }.freeze
+
+  def initialize(width: :sm)
+    @width = WIDTHS.fetch(width)
+  end
+end
