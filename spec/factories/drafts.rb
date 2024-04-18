87cf7924bdf614b9e49667554b97c92c43a6d3a0
Jeff MacDonald <j.a.macdonald@gmail.com>
2024-04-17 17:10:01 -0700

    fix factory

--- spec/factories/drafts.rb
+++ spec/factories/drafts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :draft do
     cube { create :cube }
-    user { create :user }
+    owner { create :user }
     sequence(:name) { |n| "draft#{n}" }
     status { 'PENDING' }
     rounds { 40 }
