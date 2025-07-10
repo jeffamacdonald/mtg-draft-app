0a2ad465083873448acd8d6ce3d2867913d4b34b
Jeff MacDonald <j.a.macdonald@gmail.com>
2025-07-09 22:24:33 -0700

    email next participant in queued picks job

--- app/jobs/pick_next_queued_pick_job.rb
+++ app/jobs/pick_next_queued_pick_job.rb
@@ -5,7 +5,13 @@ class PickNextQueuedPickJob < ApplicationJob
     draft_participant = participant_pick.draft_participant
     draft = draft_participant.draft
     if draft.active_pick == participant_pick && draft_participant.queue_active? && draft_participant.queued_picks.present?
-      draft_participant.pick_card!(draft_participant.queued_picks.find_by(priority_number: 1).cube_card)
+      pick = draft_participant.pick_card!(draft_participant.queued_picks.find_by(priority_number: 1).cube_card)
+
+      # Email next participant
+      next_user = draft_participant.reload.draft.active_participant.user
+      if next_user != draft_participant.user
+        PickMailer.next_up_email(pick, next_user).deliver_now
+      end
     end
   end
 end
