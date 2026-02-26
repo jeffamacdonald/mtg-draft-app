472ae426d08fe39f28392705be542c2221c167a6
Jeff MacDonald <macdonaldj@bebravehealth.com>
2026-02-25 16:58:52 -0800

    stub sassc

--- config/initializers/sassc_stub.rb
+++ config/initializers/sassc_stub.rb
@@ -1,5 +1,29 @@
-# Stub sassc module to prevent LoadError when Sprockets tries to autoload it
+# Create a stub sassc.rb file in lib directory to prevent LoadError
 # We don't actually use sassc since we handle SCSS with yarn build:css
-module SassC
-  # Empty stub module
+
+require 'fileutils'
+
+sassc_stub_path = Rails.root.join('lib', 'sassc.rb')
+FileUtils.mkdir_p(File.dirname(sassc_stub_path))
+
+unless File.exist?(sassc_stub_path)
+  File.write(sassc_stub_path, <<~RUBY)
+    # Stub sassc file to prevent LoadError when Sprockets tries to require it
+    # We handle SCSS compilation with yarn build:css instead
+    
+    module SassC
+      class Engine
+        def initialize(*)
+          # Stub implementation
+        end
+        
+        def render
+          ""
+        end
+      end
+    end
+  RUBY
 end
+
+# Add lib directory to load path so sassc.rb can be found
+$LOAD_PATH.unshift(Rails.root.join('lib')) unless $LOAD_PATH.include?(Rails.root.join('lib').to_s)
