024712185d38d71ac6d9964d5865059a9e3cfe15
Jeff MacDonald <macdonaldj@bebravehealth.com>
2026-02-25 16:49:53 -0800

    trying another approach

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,13 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-# Configure Sprockets to avoid SCSS processing since we use yarn build:css
-Rails.application.config.assets.configure do |env|
-  # Remove SCSS processors to prevent sassc dependency
-  env.unregister_mime_type('text/scss')
-  env.unregister_mime_type('text/sass')
-end
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
---
+++ config/initializers/sassc_stub.rb
@@ -0,0 +1,5 @@
+# Stub sassc module to prevent LoadError when Sprockets tries to autoload it
+# We don't actually use sassc since we handle SCSS with yarn build:css
+module SassC
+  # Empty stub module
+end
